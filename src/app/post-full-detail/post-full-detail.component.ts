36c7239af0ad8716b4d34090bbb55767cc526180
TriHoangZXC <hoanghuutri97@gmail.com>
2022-05-18 22:49:20 +0700

    add delete-comment

--- src/app/post-full-detail/post-full-detail.component.ts
+++ src/app/post-full-detail/post-full-detail.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import {Post} from "../model/Post";
-import {ActivatedRoute} from "@angular/router";
-import {PostService} from "../service/post/post.service";
+import {Component, OnInit} from '@angular/core';
+import {Post} from '../model/Post';
+import {ActivatedRoute, ParamMap, Router} from '@angular/router';
+import {PostService} from '../service/post/post.service';
 import {Comment} from '../model/comment';
 import {CommentService} from '../service/comment/comment.service';
 
@@ -14,22 +14,36 @@ export class PostFullDetailComponent implements OnInit {
 
   post: Post;
 
+  idLogin!: any;
+
+  userLogin: any;
+
+  idPost: any;
+
   comments: Comment[] = [];
 
   constructor(private route: ActivatedRoute,
-              private postService:PostService,
-              private commentService: CommentService) { }
+              private postService: PostService,
+              private commentService: CommentService,
+              private router: Router,
+              private activatedRoute: ActivatedRoute) {
+    this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
+      this.idPost = +paramMap.get('id');
+    });
+  }
 
   ngOnInit() {
+    this.userLogin = JSON.parse(localStorage.getItem('userLogin'));
+    this.idLogin = this.userLogin.id;
     this.getPostById();
   }
 
-  getPostById(){
+  getPostById() {
     const id = Number(this.route.snapshot.paramMap.get('id'));
-    this.postService.findPostById(id).subscribe((postFormBE) =>{
+    this.postService.findPostById(id).subscribe((postFormBE) => {
       this.post = postFormBE;
       this.getAllCommentByPostId(id);
-    }, error =>{
+    }, error => {
       console.log(error);
     });
   }
@@ -37,10 +51,18 @@ export class PostFullDetailComponent implements OnInit {
   getAllCommentByPostId(id: number) {
     this.commentService.getAllCommentByPostId(id).subscribe(listComment => {
       this.comments = listComment;
-      console.log("Get list comment success");
-
+      console.log('Get list comment success');
     }, error => {
-      console.log("Get list comment fail!");
-    })
+      console.log('Get list comment fail!');
+    });
+  }
+
+  deleteComment(id: number) {
+    this.commentService.deleteCommentPost(id).subscribe(() => {
+      console.log('Delete comment success');
+      this.getPostById();
+    }, error => {
+      console.log('Delete comment fail');
+    });
   }
 }
