3ae15e2e3923d14b9ba4fdcab08fc51f11e6d000
SangBui <sangnhanma14@gmail.com>
2022-05-19 08:43:35 +0700

    secord commit

--- src/app/security/login/login.component.ts
+++ src/app/security/login/login.component.ts
@@ -125,96 +125,97 @@ export class LoginComponent implements OnInit {
 
   }
 
-  // response;
-  // socialusers = new SocialUser();
-  //
-  // login() {
-  //   // this.auth.signInWithPopup();
-  //   this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider()).then(result => {
-  //     // The firebase.User instance:
-  //     const user = result.user;
-  //     // The Facebook firebase.auth.AuthCredential containing the Facebook
-  //     // access token:
-  //     const credential = result.credential;
-  //     console.log(user);
-  //     console.log(credential);
-  //     this.authService.signInSocial({type: 'google', email: user.email}).subscribe(resp => {
-  //       console.log(resp, 'resp');
-  //       if (resp.token != undefined) {
-  //         this.tokenService.setToken(resp.token);
-  //         this.tokenService.setName(resp.fullName);
-  //         this.name = this.tokenService.getName();
-  //         this.tokenService.setRoles(resp.roles);
-  //         this.isLoginFailed = false;
-  //         this.isLoggedIn = true;
-  //         this.nameLogin = resp.fullName;
-  //         for (let i = 0; i < resp.roles.length; i++) {
-  //           if (resp.roles[i]['authority'] === 'ADMIN') {
-  //             this.role = Role.Admin;
-  //           } else if (resp.roles[i]['authority'] === 'USER') {
-  //             this.role = Role.User;
-  //           }
-  //         }
-  //         localStorage.setItem("userLogin", JSON.stringify(this.user));
-  //         localStorage.setItem("nameLogin", this.nameLogin);
-  //         localStorage.setItem("roleLogin", this.role);
-  //         this.router.navigate(['']).then(() => {
-  //           window.location.reload();
-  //         });
-  //
-  //       }
-  //       localStorage.setItem('socialusers', JSON.stringify(resp));
-  //       this.router.navigate(['']).then(() => {
-  //         window.location.reload();
-  //       });
-  //     });
-  //   }, error => {
-  //     // The provider's account email, can be used in case of
-  //     // auth/account-exists-with-different-credential to fetch the providers
-  //     // linked to the email:
-  //     var email = error.email;
-  //     // The provider's credential:
-  //     var credential = error.credential;
-  //     // In case of auth/account-exists-with-different-credential error,
-  //     // you can fetch the providers using this:
-  //     // if (error.code === 'auth/account-exists-with-different-credential') {
-  //     //   this.afAuth.auth.fetchSignInMethodsForEmail(email).then(function(providers) {
-  //     //     // The returned 'providers' is a list of the available providers
-  //     //     // linked to the email address. Please refer to the guide for a more
-  //     //     // complete explanation on how to recover from this error.
-  //     //   });
-  //     // }
-  //   });
-  // }
-  //
-  // public socialSignIn(socialProvider: string) {
-  //   console.log(socialProvider)
-  //   let socialPlatformProvider;
-  //   if (socialProvider === 'facebook') {
-  //     socialPlatformProvider = FacebookLoginProvider.PROVIDER_ID;
-  //   } else if (socialProvider === 'google') {
-  //     socialPlatformProvider = GoogleLoginProvider.PROVIDER_ID;
-  //   }
-  //
-  //   this.authService.signIn(socialPlatformProvider).subscribe(socialusers => {
-  //     console.log(socialProvider, socialusers);
-  //     console.log(socialusers);
-  //     this.Savesresponse(socialusers);
-  //
-  //   });
-  // }
-  //
-  // Savesresponse(socialusers: JwtRespone) {
-  //
-  //   this.socialService.Savesresponse(socialusers).subscribe((res: any) => {
-  //     debugger;
-  //     console.log(res);
-  //     this.socialusers=res;
-  //     this.response = res.userDetail;
-  //     localStorage.setItem('socialusers', JSON.stringify( this.socialusers));
-  //     console.log(localStorage.setItem('socialusers', JSON.stringify(this.socialusers)));
-  //     this.router.navigate([``]);
-  //   })
-  // }
+  response;
+  socialusers = new SocialUser();
+
+  login() {
+    // this.auth.signInWithPopup();
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider()).then(result => {
+      // The firebase.User instance:
+      const user = result.user;
+      // The Facebook firebase.auth.AuthCredential containing the Facebook
+      // access token:
+      const credential = result.credential;
+      console.log(user);
+      console.log(credential);
+      this.authService.signInSocial({type: 'google', email: user.email}).subscribe(resp => {
+        console.log(resp, 'resp');
+        // tslint:disable-next-line:triple-equals
+        if (resp.token != undefined) {
+          this.tokenService.setToken(resp.token);
+          this.tokenService.setName(resp.fullName);
+          this.name = this.tokenService.getName();
+          this.tokenService.setRoles(resp.roles);
+          this.isLoginFailed = false;
+          this.isLoggedIn = true;
+          this.nameLogin = resp.fullName;
+          for (let i = 0; i < resp.roles.length; i++) {
+            if (resp.roles[i]['authority'] === 'ADMIN') {
+              this.role = Role.Admin;
+            } else if (resp.roles[i]['authority'] === 'USER') {
+              this.role = Role.User;
+            }
+          }
+          localStorage.setItem("userLogin", JSON.stringify(this.user));
+          localStorage.setItem("nameLogin", this.nameLogin);
+          localStorage.setItem("roleLogin", this.role);
+          this.router.navigate(['']).then(() => {
+            window.location.reload();
+          });
+
+        }
+        localStorage.setItem('socialusers', JSON.stringify(resp));
+        this.router.navigate(['']).then(() => {
+          window.location.reload();
+        });
+      });
+    }, error => {
+      // The provider's account email, can be used in case of
+      // auth/account-exists-with-different-credential to fetch the providers
+      // linked to the email:
+      var email = error.email;
+      // The provider's credential:
+      var credential = error.credential;
+      // In case of auth/account-exists-with-different-credential error,
+      // you can fetch the providers using this:
+      if (error.code === 'auth/account-exists-with-different-credential') {
+        this.afAuth.auth.fetchSignInMethodsForEmail(email).then(function(providers) {
+      //     // The returned 'providers' is a list of the available providers
+      //     // linked to the email address. Please refer to the guide for a more
+      //     // complete explanation on how to recover from this error.
+        });
+      }
+    });
+  }
+
+  public socialSignIn(socialProvider: string) {
+    console.log(socialProvider)
+    let socialPlatformProvider;
+    if (socialProvider === 'facebook') {
+      socialPlatformProvider = FacebookLoginProvider.PROVIDER_ID;
+    } else if (socialProvider === 'google') {
+      socialPlatformProvider = GoogleLoginProvider.PROVIDER_ID;
+    }
+
+    this.authService.signIn(socialPlatformProvider).subscribe(socialusers => {
+      console.log(socialProvider, socialusers);
+      console.log(socialusers);
+      this.Savesresponse(socialusers);
+
+    });
+  }
+
+  Savesresponse(socialusers: JwtRespone) {
+
+    this.socialService.Savesresponse(socialusers).subscribe((res: any) => {
+      debugger;
+      console.log(res);
+      this.socialusers=res;
+      this.response = res.userDetail;
+      localStorage.setItem('socialusers', JSON.stringify( this.socialusers));
+      console.log(localStorage.setItem('socialusers', JSON.stringify(this.socialusers)));
+      this.router.navigate([``]);
+    })
+  }
 
 }
